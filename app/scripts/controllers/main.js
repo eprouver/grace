70521820cce9b8859755567a1494ebfd64b51877
= <eprouver@yahoo.com>
2016-09-23 10:12:43 -0300

    angular

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name graceApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the graceApp
+ */
+angular.module('graceApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
